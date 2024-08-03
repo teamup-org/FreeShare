a8c94fc00c046dfd224d8ff658398e971c3020e9
Grant Lipham <58563668+RobotGR@users.noreply.github.com>
2024-08-03 17:44:15 -0500

    Initialize readwell-student-frontend-show-hw

---
+++ src/client/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './HomePage';
+import ProfileGallery from './ProfileGallery';
+import Profile from './Profile';
+import NotFoundPage from './NotFoundPage';
+import AIProfile from './AIProfile';
+import StudentAssignments from './StudentAssignments';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile-gallery" element={<ProfileGallery />} />
+          <Route path="/ai-profile" element={<AIProfile />} />
+          <Route path="/student-assignments" element={<StudentAssignments />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
