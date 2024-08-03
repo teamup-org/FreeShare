81cedce4cb2366f10319042d46e37178a0dc0a65
Grant Lipham <58563668+RobotGR@users.noreply.github.com>
2024-08-03 17:44:14 -0500

    Initialize ai-profile-summary-helper

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const plugins = [];
+if (process.env.NODE_ENV === 'development') {
+  plugins.push('react-refresh/babel');
+}
+
+module.exports = (api) => {
+  // This caches the Babel config
+  api.cache.using(() => process.env.NODE_ENV);
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-typescript',
+      // Enable development transform of React with new automatic runtime
+      ['@babel/preset-react', { development: !api.env('production'), runtime: 'automatic' }],
+    ],
+    // Applies the react-refresh Babel plugin on non-production modes only
+    ...(!api.env('production') && { plugins }),
+  };
+};
+  
---
+++ src/client/AIProfile.tsx
@@ -0,0 +1,12 @@
+import ProfileDescription from "./ProfileDescription";
+
+const AIProfile = () => {
+    return (
+      <div>
+        <h1>AI Profile Summary Page</h1>
+        <ProfileDescription />
+      </div>
+    );
+  }
+  
+  export default AIProfile;
