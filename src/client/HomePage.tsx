ede86810afe9abcd2fd2bf5412c123780ed1abbd
RobotGR <grantlipham12@gmail.com>
2024-08-09 16:08:50 -0500

    Spelling

--- src/client/HomePage.tsx
+++ src/client/HomePage.tsx
@@ -138,7 +138,7 @@ const HomePage = () => {
             <div>
               <p className="description-text">
                 {" "}
-                An open market for studetns to exchange unwanted items
+                An open market for students to exchange unwanted items
               </p>
             </div>
           </div>
